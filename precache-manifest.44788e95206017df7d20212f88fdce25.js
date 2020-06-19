d67086f2c4c77ec3d9fca93deb21371f34c3dbb9
Jonathon Slootweg <slootwegj@gmail.com>
2020-06-18 17:17:09 -0700

    Updates

---
+++ precache-manifest.44788e95206017df7d20212f88fdce25.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c06d5a5aebbdc40f7ac0afa5eca034",
+    "url": "/roller/index.html"
+  },
+  {
+    "revision": "f24199278aca24006b15",
+    "url": "/roller/static/css/main.5e34062c.chunk.css"
+  },
+  {
+    "revision": "fb0bb4403d97fb36f8eb",
+    "url": "/roller/static/js/2.e5acb45d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roller/static/js/2.e5acb45d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24199278aca24006b15",
+    "url": "/roller/static/js/main.974623df.chunk.js"
+  },
+  {
+    "revision": "4c4c6f436bb88b7ca8e5",
+    "url": "/roller/static/js/runtime-main.78e0f0ba.js"
+  },
+  {
+    "revision": "04278970f3333b4563f4af9cf5eb236f",
+    "url": "/roller/static/media/2-Dice-Icon.04278970.svg"
+  },
+  {
+    "revision": "3e0ba75a1d2cca6c7f31a4776ed80f61",
+    "url": "/roller/static/media/arrow-up-circle.3e0ba75a.svg"
+  },
+  {
+    "revision": "876e4448856b0af5f7ebd927f67c4e59",
+    "url": "/roller/static/media/homemade-dice.876e4448.svg"
+  },
+  {
+    "revision": "a4eb83269dbac772999d87f91a5278ea",
+    "url": "/roller/static/media/pause-circle.a4eb8326.svg"
+  },
+  {
+    "revision": "63d6952e6b9191f8d0d5d7daecd53f62",
+    "url": "/roller/static/media/play-circle.63d6952e.svg"
+  },
+  {
+    "revision": "8b81b4b8ef0cbd59967a8516c5e0e428",
+    "url": "/roller/static/media/refresh-cw.8b81b4b8.svg"
+  }
+]);
